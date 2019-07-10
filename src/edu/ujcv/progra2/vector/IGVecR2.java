fa1440f8ad2bc34b5aecca324a4b28d1ff06e9d9
VikoSvia <coto1808@gmail.com>
2019-07-09 23:03:29 -0600

    Se termina la parte de IGVecR2

--- src/edu/ujcv/progra2/vector/IGVecR2.java
+++ src/edu/ujcv/progra2/vector/IGVecR2.java
@@ -6,47 +6,62 @@ public class IGVecR2 {
     private double x;
     private double y;
 
-    public IGVecR2 suma(IGVecR2 b){
+    public IGVecR2 suma (IGVecR2 b){
 
         IGVecR2 retval = new IGVecR2();
         retval.x = x + b.x;
         retval.y = y + b.y;
         System.out.println(retval);
 
-        return new IGVecR2();
+        return new retval;
     }
 
-    public IGVecR2 resta(IGVecR2 b){
+    public IGVecR2 resta (IGVecR2 b){
           IGVecR2 retval = new IGVecR2();
           retval.x = x - b.x;
           retval.y = y - b.y;
-        return new IGVecR2();
+        return new retval;
     }
-    
-    public  IGVecR2 escalarPorVector(double alpha){
-        //TODO: implementar
-        return new IGVecR2();
+
+    public  IGVecR2 escalarPorVector (double alpha){
+        IGVecR2 retval = new IGVecR2();
+        retval.x = (alpha * x);
+        retval.y = (alpha * y);
+        return new retval;
     }
 
     public double productoPunto(IGVecR2 b){
-        //TODO: implementar
-        return 0.0;
+        double retval;
+        retval = (x * b.x) + (y * b.y);
+        return retval;
     }
 
 
-    // no esta definido en R2.!!!!!
-    //public IGVecR2 productoCruz(IGVecR2 b){
-
-    //}
-
-    public double angulo(IGVecR2 b){
-        //TODO: implementar
-        return 0.0;
+    public double angulo (IGVecR2 b, IGVecR2 a){
+        double retval;
+        retval = Math.acos(a.productoPunto(b)/ magnitud(a) * magnitud(b));
+        return Math.toDegrees(retval);
     }
 
-    public double magnitud(){
-        return Math.sqrt(productoPunto(this));
+    public double magnitud(IGVecR2 b) {
+        return Math.sqrt(Math.pow(b.x,2)) + Math.pow(b.y,2);
     }
 
 
+
+public double getX(){
+return x;
+}
+
+public void setX(double x){
+    this.x = x;
+}
+
+public double getY(){
+    return y;
+}
+
+public void setY(double y){
+    this.y = y;
+}
 }
