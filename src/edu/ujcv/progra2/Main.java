f5f874da8d60e33cfbcc6953128a6e02f24f073e
VikoSvia <coto1808@gmail.com>
2019-07-10 03:12:33 -0600

    Se empieza a trabajar en el main

--- src/edu/ujcv/progra2/Main.java
+++ src/edu/ujcv/progra2/Main.java
@@ -1,8 +1,42 @@
 package edu.ujcv.progra2;
 
+import edu.ujcv.progra2.matriz.IGMatR2;
+import edu.ujcv.progra2.matriz.IGMatR3;
+import edu.ujcv.progra2.matriz.IGMatR4;
+import edu.ujcv.progra2.vector.IGVecR2;
+import edu.ujcv.progra2.vector.IGVecR3;
+import edu.ujcv.progra2.vector.IGVecR4;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        IGVecR2 vector2 = new IGVecR2();
+        IGVecR3 vector3 = new IGVecR3();
+        IGVecR4 vector4 = new IGVecR4();
+        IGMatR2 matriz2 = new IGMatR2();
+        IGMatR3 matriz3 = new IGMatR3();
+        IGMatR4 matriz4 = new IGMatR4();
+        long opcion1, opcion2, ;
+
+        System.out.println("Bienvenido a la Calculadora Suprema de Vectores y Matrices 20000");
+        System.out.println("Ingresa si quieres Operaciones Vectoriales o Operaciones Matriciales");
+        System.out.println("1) Vectoriales");
+        System.out.println("2) Matriciales");
+        System.out.print("Opcion: ");
+        opcion1 = sc.nextInt();
+
+        if (opcion1 == 1){
+            System.out.println("Seleccione que ");
+        }
+
+        if (opcion1 == 2){
+
+        }
+
+
     }
 }
