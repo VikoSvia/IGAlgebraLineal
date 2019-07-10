70c4542a79e7ece5e931b3377e99ae3b16852427
VikoSvia <coto1808@gmail.com>
2019-07-10 00:48:04 -0600

    Se termina el IGVecR3

--- src/edu/ujcv/progra2/vector/IGVecR3.java
+++ src/edu/ujcv/progra2/vector/IGVecR3.java
@@ -58,5 +58,29 @@ public class IGVecR3 {
     }
 
 
+public double getX(){
+   return x;
+    }
 
+public void setX(double x){
+        this.x = x;
+    }
+
+public double getY() {
+   return y;
+    }
+
+public void setY(double y) {
+   this.y = y;
+    }
+
+public double getZ() {
+   return z;
+    }
+
+public void setZ(double z) {
+   this.z = z;
+    }
 }
+
+
