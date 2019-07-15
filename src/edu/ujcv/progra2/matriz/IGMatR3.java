3359e6823d8b303cf2615071c883ebb82495ca2b
VikoSvia <coto1808@gmail.com>
2019-07-14 22:56:33 -0600

    Se agrega la rotacion z y se empieza la rotacion x en IGMatR3

--- src/edu/ujcv/progra2/matriz/IGMatR3.java
+++ src/edu/ujcv/progra2/matriz/IGMatR3.java
@@ -12,7 +12,22 @@ public class IGMatR3 {
 
         retval.colum1.setX(this.colum1.getX() * Math.cos(angulo));
         retval.colum1.setY(this.colum1.getY() * Math.sin(angulo));
-        
+        retval.colum1.setZ(this.colum1.getZ() * 0);
+        retval.colum2.setX(this.colum2.getX() * -Math.sin(angulo));
+        retval.colum2.setY(this.colum2.getY() * Math.cos(angulo));
+        retval.colum2.setZ(this.colum2.getZ() * 0);
+        retval.colum3.setX(this.colum3.getX() * 0);
+        retval.colum3.setY(this.colum3.getY() * 0);
+        retval.colum3.setZ(this.colum3.getZ() * 1);
+
+        return retval;
+    }
+
+    public IGMatR3 rotacionX (double angulo, IGVecR3 vector){
+        IGMatR3 retval = new IGMatR3();
+
+        retval.colum1.setX(this.colum1.getX() * 1);
+
 
         return retval;
     }
