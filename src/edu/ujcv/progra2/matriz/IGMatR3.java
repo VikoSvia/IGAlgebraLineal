ec0e28fee4e74fdb8e55ab0f9fb4479061ae689c
VikoSvia <coto1808@gmail.com>
2019-07-14 23:21:06 -0600

    Se termina la rotacion x en IGMatR3

--- src/edu/ujcv/progra2/matriz/IGMatR3.java
+++ src/edu/ujcv/progra2/matriz/IGMatR3.java
@@ -27,6 +27,14 @@ public class IGMatR3 {
         IGMatR3 retval = new IGMatR3();
 
         retval.colum1.setX(this.colum1.getX() * 1);
+        retval.colum1.setY(this.colum1.getY() * 0);
+        retval.colum1.setZ(this.colum1.getZ() * 0);
+        retval.colum3.setX(this.colum2.getX() * 0);
+        retval.colum3.setY(this.colum2.getY() * Math.cos(angulo));
+        retval.colum3.setZ(this.colum2.getZ() * Math.sin(angulo));
+        retval.colum2.setX(this.colum2.getX() * 0);
+        retval.colum2.setY(this.colum2.getY() * -Math.sin(angulo));
+        retval.colum2.setZ(this.colum2.getZ() * Math.cos(angulo));
 
 
         return retval;
